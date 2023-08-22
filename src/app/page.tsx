799cd569f60ca956a548bbbbfb04934078aedd1b
Amalya Megerman <amalyamegerman@gmail.com>
2023-08-21 20:17:00 -0400

    add photo images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import ContactForm from "./components/ContactForm";
+import Image from "next/image";
+import LogoAndServices from "/src/images/logo-and-services.jpg";
+import CleanRoof from "/src/images/clean-roof.jpg";
+
+/* <Image
+src={LogoAndServices}
+width={781.5}
+height={374}
+alt="logo-and-services"
+placeholder="blur"
+/> */
 
 export default function Home() {
   return (
     <div>
-      HomeShine RoofPro
+      <Image src={CleanRoof} alt="clean roof" placeholder="blur" />
       <ContactForm />
     </div>
   );
