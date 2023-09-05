79cb38f63dd217e37e25761c66cd762fe5088319
Amalya Megerman <amalyamegerman@gmail.com>
2023-09-05 10:57:26 -0400

    create basic component

---
+++ src/app/components/ComingSoon.tsx
@@ -0,0 +1,10 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+export default function ComingSoon() {
+  return (
+    <Container>
+      <Box></Box>
+    </Container>
+  );
+}
