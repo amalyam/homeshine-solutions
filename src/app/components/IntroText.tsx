1b305cf9560b91a43fb0342b65e8b237500f883b
Amalya Megerman <amalyamegerman@gmail.com>
2023-09-05 20:42:13 -0400

    update layout, change breakpoint sizing

--- src/app/components/IntroText.tsx
+++ src/app/components/IntroText.tsx
@@ -1,3 +1,4 @@
+import { List, ListItemText } from "@mui/material";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
@@ -5,16 +6,40 @@ import Typography from "@mui/material/Typography";
 
 export default function IntroText() {
   return (
-    <Container sx={{ m: 3, display: "flex" }}>
-      <Paper sx={{ backgroundColor: "rgba(255, 255, 255, 0.75)" }}>
-        <Box>
-          <Typography fontWeight={700} color="inherit">
-            Protect your home with professional routine maintenance. We provide
-            meticulous cleaning, guaranteed to extend the life of your home. We
-            specialize in: Gutters Moss Treatment Solar panels Soft wash Window
-            Cleaning
-          </Typography>
-        </Box>
+    <Container
+      sx={{
+        m: 1,
+        marginTop: 10,
+        p: 3,
+        display: "flex",
+        blockSize: "fit-content",
+        backgroundColor: "rgba(83, 208, 254, 0.5)",
+        width: { xs: "100%", sm: "60%", lg: "80%" },
+      }}
+    >
+      <Paper
+        sx={{ p: 2, blockSize: "fit-content", backgroundColor: "primary" }}
+      >
+        <Typography
+          fontWeight={700}
+          color="inherit"
+          textAlign="center"
+          textTransform={"uppercase"}
+        >
+          Protect your home with professional routine maintenance!
+        </Typography>
+        <br />
+        <Typography fontWeight={700} color="inherit" textAlign="center">
+          We provide meticulous cleaning, guaranteed to extend the life of your
+          home. We specialize in:
+          <List>
+            <ListItemText>Gutters</ListItemText>
+            <ListItemText>Moss treatment</ListItemText>
+            <ListItemText>Solar panels</ListItemText>
+            <ListItemText>Soft wash</ListItemText>
+            <ListItemText>Window cleaning</ListItemText>
+          </List>
+        </Typography>
       </Paper>
     </Container>
   );
