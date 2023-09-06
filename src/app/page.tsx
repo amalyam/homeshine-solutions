137576b3ecc35d90fa56da3473dc23ca8e0e5517
Amalya Megerman <amalyamegerman@gmail.com>
2023-09-05 20:44:09 -0400

    add Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import ContactForm from "./components/ContactForm";
-import Image from "next/image";
-import LogoAndServices from "/src/images/logo-and-services.jpg";
 import CleanRoof from "/src/images/clean-roof.jpg";
 import IntroText from "./components/IntroText";
+import Grid from "@mui/material/Unstable_Grid2";
 
 /* 
-TODO create color theme based on Homeshine logo
+TODO 
+get  background image to cover full length of contact form
+
+create color theme based on Homeshine logo
 primary white
 teal
 yellow
@@ -22,11 +24,20 @@ placeholder="blur"
 
 export default function Home() {
   return (
-    <div style={{ backgroundImage: `url(${CleanRoof.src})`, height: "100vh" }}>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <IntroText />
-        <ContactForm />
-      </div>
+    <div
+      style={{
+        backgroundImage: `url(${CleanRoof.src})`,
+        height: "100vh",
+      }}
+    >
+      <Grid container spacing={2} display={"flex"} m={3}>
+        <Grid xs={12} sm={6}>
+          <IntroText />
+        </Grid>
+        <Grid xs={12} sm={6}>
+          <ContactForm />
+        </Grid>
+      </Grid>
     </div>
   );
 }
