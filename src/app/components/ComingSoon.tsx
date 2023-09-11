ce00bb34ab4fd96129e601b8559af9522a789fea
Amalya Megerman <amalyamegerman@gmail.com>
2023-09-11 14:50:52 -0400

    fix alignment issues

--- src/app/components/ComingSoon.tsx
+++ src/app/components/ComingSoon.tsx
@@ -1,15 +1,32 @@
-import Image from "next/image";
+"use client";
+
+import Box from "@mui/material/Box";
 import MossHand from "/src/images/moss-hand-2.jpg";
-import { calculateSizeAdjustValues } from "next/dist/server/font-utils";
+import Typography from "@mui/material/Typography";
+import { alpha } from "@mui/material";
+import theme from "../theme";
 
 export default function ComingSoon() {
   return (
     <div
       style={{
+        display: "flex",
         flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
         height: "calc(100vh - 84px)",
         background: `no-repeat top/cover url(${MossHand.src})`,
       }}
-    ></div>
+    >
+      <Box
+        sx={{
+          backgroundColor: alpha(theme.palette.grey[500], 0.5),
+        }}
+      >
+        <Typography fontSize="8rem" color="white">
+          COMING SOON
+        </Typography>
+      </Box>
+    </div>
   );
 }
