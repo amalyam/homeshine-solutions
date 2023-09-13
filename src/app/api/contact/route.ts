86fc529ea79d4499ace4978e189c08a48b8d8500
Amalya Megerman <amalyamegerman@gmail.com>
2023-09-12 22:49:45 -0400

    connect to backend

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,28 @@
+import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 import FormFields from "../../types/FormFields";
 
 export async function POST(request: NextRequest) {
-  const body = (await request.json()) as { data: FormFields };
-  console.log("Contact route received: ", body);
+  const {
+    data: {
+      name,
+      email,
+      phone,
+      address,
+      zip,
+      message,
+      services,
+      referralSource,
+    },
+  } = (await request.json()) as { data: FormFields };
 
+  const result = await sql`
+  INSERT INTO contact (name, email, phone, address, zip, services, referral_source, message)
+  VALUES (${name}, ${email}, ${phone}, ${address}, ${zip}, ${JSON.stringify(
+    services
+  )}, ${JSON.stringify(referralSource)}, ${message});
+  `;
+
+  console.log(result);
   return NextResponse.json({ success: true });
 }
