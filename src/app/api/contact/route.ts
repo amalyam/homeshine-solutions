2983aa05d9073da7f6368f527c93f1759258657c
Amalya Megerman <amalyamegerman@gmail.com>
2023-09-12 22:50:56 -0400

    add contact data to Sheet

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,8 +1,24 @@
 import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 import FormFields from "../../types/FormFields";
+import { google, Auth } from "googleapis";
+
+const googleService = google.sheets({
+  version: "v4",
+  auth: new google.auth.GoogleAuth({
+    credentials: {
+      type: "service_account",
+      quota_project_id: "homeshine-website",
+      client_id: process.env.GOOGLE_CLOUD_CLIENT_ID,
+      client_email: process.env.GOOGLE_CLOUD_EMAIL,
+      private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY,
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  }),
+});
 
 export async function POST(request: NextRequest) {
+  console.log("request to contact route");
   const {
     data: {
       name,
@@ -16,13 +32,40 @@ export async function POST(request: NextRequest) {
     },
   } = (await request.json()) as { data: FormFields };
 
-  const result = await sql`
-  INSERT INTO contact (name, email, phone, address, zip, services, referral_source, message)
-  VALUES (${name}, ${email}, ${phone}, ${address}, ${zip}, ${JSON.stringify(
-    services
-  )}, ${JSON.stringify(referralSource)}, ${message});
-  `;
+  // const result = await sql`
+  // INSERT INTO contact (name, email, phone, address, zip, services, referral_source, message)
+  // VALUES (${name}, ${email}, ${phone}, ${address}, ${zip}, ${JSON.stringify(
+  //   services
+  // )}, ${JSON.stringify(referralSource)}, ${message});
+  // `;
 
-  console.log(result);
+  await googleService.spreadsheets.values.append({
+    spreadsheetId: "1HpxH5RSSLOrpKdRCwm7h0Ndneo2SSMBB2UOhS0Yl3QM", // from the URL
+    range: process.env.CONTACT_SHEET_NAME, // or whatever other sheet name you name it
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [
+        [
+          new Date().toLocaleString("en-US", {
+            timeZone: "America/New_York",
+          }),
+          name,
+          email,
+          phone,
+          address,
+          zip,
+          services.join(","),
+          referralSource.join(", "),
+          message,
+        ],
+      ], // fill with the data you're inserting
+    },
+  });
+  /*
+
+  */
+
+  console.log("success");
+  // console.log(result);
   return NextResponse.json({ success: true });
 }
