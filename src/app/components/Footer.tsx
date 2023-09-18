236ba932a986113444eddff3a22c42a97192f1ce
Amalya Megerman <amalyamegerman@gmail.com>
2023-09-17 23:12:31 -0400

    layout changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -37,46 +37,52 @@ export default function AppFooter() {
       sx={{ display: "flex", bgcolor: "secondary.light" }}
     >
       <Container sx={{ my: 2, display: "flex" }}>
-        <Grid container spacing={5}>
-          <Grid item xs={6} sm={4} md={3}>
-            <Grid
-              container
-              direction="column"
-              justifyContent="flex-end"
-              spacing={2}
-              sx={{ height: 120 }}
-            >
-              <Grid item sx={{ display: "flex" }}>
-                <Link
-                  href="https://www.yelp.com/biz/home-shine-solutions-somerville"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="1.5em"
-                    viewBox="0 0 384 512"
-                  >
-                    <path d="M42.9 240.32l99.62 48.61c19.2 9.4 16.2 37.51-4.5 42.71L30.5 358.45a22.79 22.79 0 0 1-28.21-19.6 197.16 197.16 0 0 1 9-85.32 22.8 22.8 0 0 1 31.61-13.21zm44 239.25a199.45 199.45 0 0 0 79.42 32.11A22.78 22.78 0 0 0 192.94 490l3.9-110.82c.7-21.3-25.5-31.91-39.81-16.1l-74.21 82.4a22.82 22.82 0 0 0 4.09 34.09zm145.34-109.92l58.81 94a22.93 22.93 0 0 0 34 5.5 198.36 198.36 0 0 0 52.71-67.61A23 23 0 0 0 364.17 370l-105.42-34.26c-20.31-6.5-37.81 15.8-26.51 33.91zm148.33-132.23a197.44 197.44 0 0 0-50.41-69.31 22.85 22.85 0 0 0-34 4.4l-62 91.92c-11.9 17.7 4.7 40.61 25.2 34.71L366 268.63a23 23 0 0 0 14.61-31.21zM62.11 30.18a22.86 22.86 0 0 0-9.9 32l104.12 180.44c11.7 20.2 42.61 11.9 42.61-11.4V22.88a22.67 22.67 0 0 0-24.5-22.8 320.37 320.37 0 0 0-112.33 30.1z" />
-                  </svg>
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={6} sm={4} md={2}>
-            <Typography variant="h6" gutterBottom sx={{ my: 2 }}>
-              Legal
-            </Typography>
-            <Box component="ul" sx={{ m: 0, listStyle: "none", p: 0 }}>
-              <Box component="li" sx={{ py: 0.5 }}>
-                <Link href="/Privacy/">Privacy</Link>
-              </Box>
+        <Link
+          href="https://www.yelp.com/biz/home-shine-solutions-somerville"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1.5em"
+            viewBox="0 0 384 512"
+          >
+            <path d="M42.9 240.32l99.62 48.61c19.2 9.4 16.2 37.51-4.5 42.71L30.5 358.45a22.79 22.79 0 0 1-28.21-19.6 197.16 197.16 0 0 1 9-85.32 22.8 22.8 0 0 1 31.61-13.21zm44 239.25a199.45 199.45 0 0 0 79.42 32.11A22.78 22.78 0 0 0 192.94 490l3.9-110.82c.7-21.3-25.5-31.91-39.81-16.1l-74.21 82.4a22.82 22.82 0 0 0 4.09 34.09zm145.34-109.92l58.81 94a22.93 22.93 0 0 0 34 5.5 198.36 198.36 0 0 0 52.71-67.61A23 23 0 0 0 364.17 370l-105.42-34.26c-20.31-6.5-37.81 15.8-26.51 33.91zm148.33-132.23a197.44 197.44 0 0 0-50.41-69.31 22.85 22.85 0 0 0-34 4.4l-62 91.92c-11.9 17.7 4.7 40.61 25.2 34.71L366 268.63a23 23 0 0 0 14.61-31.21zM62.11 30.18a22.86 22.86 0 0 0-9.9 32l104.12 180.44c11.7 20.2 42.61 11.9 42.61-11.4V22.88a22.67 22.67 0 0 0-24.5-22.8 320.37 320.37 0 0 0-112.33 30.1z" />
+          </svg>
+        </Link>
+        <Box>
+          <Typography
+            gutterBottom
+            sx={{
+              my: 2,
+              display: "block",
+              marginTop: 0.5,
+              fontWeight: 700,
+              fontSize: "1em",
+              textTransform: "uppercase",
+            }}
+          >
+            Legal
+            <span
+              style={{
+                height: 2,
+                width: 28,
+                display: "block",
+                marginTop: 0.5,
+                background: "currentColor",
+              }}
+            ></span>
+          </Typography>
+          <Box component="ul" sx={{ m: 0, listStyle: "none", p: 0 }}>
+            <Box component="li" sx={{ py: 0.25 }}>
+              <Link href="/Privacy/">Privacy</Link>
             </Box>
-            <Grid item>
-              <Copyright />
-            </Grid>
-          </Grid>
-        </Grid>
+            <Box component="li" sx={{ py: 0.25 }}>
+              <Link href="/Privacy/">Terms & Conditions</Link>
+            </Box>
+          </Box>
+        </Box>
+        <Copyright />
       </Container>
     </Typography>
   );
