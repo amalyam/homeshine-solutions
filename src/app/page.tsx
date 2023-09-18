70a4d8a49cf33e2941f61c74061573418fdfe891
Amalya Megerman <amalyamegerman@gmail.com>
2023-09-18 14:26:56 -0400

    add flexGrow: 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,25 +24,24 @@ placeholder="blur"
 
 export default function Home() {
   return (
-    <div>
-      <Box
-        sx={{
-          backgroundImage: `url(${CleanRoof.src})`,
-          backgroundSize: "stretch",
-          display: "flex",
-          flexDirection: {
-            xs: "column",
-            lg: "row",
-          },
-          justifyContent: "space-around",
-          height: "100%",
-          alignItems: "center",
-          zIndex: "1000",
-        }}
-      >
-        <IntroText />
-        <ContactForm />
-      </Box>
-    </div>
+    <Box
+      sx={{
+        backgroundImage: `url(${CleanRoof.src})`,
+        backgroundSize: "stretch",
+        display: "flex",
+        flexDirection: {
+          xs: "column",
+          lg: "row",
+        },
+        justifyContent: "space-around",
+        height: "100%",
+        alignItems: "center",
+        zIndex: "1000",
+        flexGrow: "1",
+      }}
+    >
+      <IntroText />
+      <ContactForm />
+    </Box>
   );
 }
