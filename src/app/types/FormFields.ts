4e2c7c136cd39a1e1990a3555e8b47611fbbfa11
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-10-16 21:28:33 +0000

    correct optional fields in contact form

--- src/app/types/FormFields.ts
+++ src/app/types/FormFields.ts
@@ -1,10 +1,10 @@
 export default interface FormFields {
   name: string;
   email: string;
-  phone: string;
-  address: string;
+  phone?: string;
+  address?: string;
   zip: string;
-  services: string[];
-  referralSource: string[];
+  services?: string[];
+  referralSource?: string[];
   message: string;
 }
