d7aba9d711092d831d1e117a167b6aebbf5835de
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-10-30 19:46:56 +0000

    extract logo, privacy, and terms to a component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,131 +10,48 @@ import SudsHome from "/src/images/suds-home-transparent.svg";
 import Typography from "@mui/material/Typography";
 import theme from "../theme";
 
-function Copyright() {
+function LogoPrivacyTerms() {
   return (
-    <div>
-      ©{new Date().getFullYear()} HomeShine Solutions, LLC. All rights reserved.
-    </div>
-  );
-}
-
-export default function AppFooter() {
-  return (
-    <Typography
-      component="footer"
-      sx={{
-        bgcolor: theme.palette.footer.light,
-        py: 1.5,
-        fontSize: ".75rem",
-      }}
-    >
-      <Grid
-        container
-        sx={{
+    <>
+      <Image
+        src={SudsHome}
+        alt={"Suds Home Logo"}
+        height={40}
+        style={{
           display: "flex",
-          justifyContent: "space-between",
           alignItems: "center",
+          marginRight: "10px",
+        }}
+      />
+      <Link
+        href="/Privacy/"
+        color={theme.palette.footer.dark}
+        underline="none"
+        sx={{
+          "&:hover": {
+            color: theme.palette.footer.main,
+          },
         }}
       >
-        <Grid
-          xs={4}
-          sx={{
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            px: 3,
-          }}
-        >
-          <Image
-            src={SudsHome}
-            alt={"Suds Home Logo"}
-            height={40}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              marginRight: "10px",
-            }}
-          />
-          <Link
-            href="/Privacy/"
-            color={theme.palette.footer.dark}
-            underline="none"
-            sx={{
-              "&:hover": {
-                color: theme.palette.footer.main,
-              },
-            }}
-          >
-            Privacy Policy
-          </Link>
-          <Typography sx={{ mx: 1 }}>|</Typography>
-          <Link
-            href="/Terms/"
-            color={theme.palette.footer.dark}
-            underline="none"
-            sx={{
-              "&:hover": {
-                color: theme.palette.footer.main,
-              },
-            }}
-          >
-            Terms & Conditions
-          </Link>
-        </Grid>
-        <Grid
-          xs={4}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 1.5,
-          }}
-        >
-          <Typography
-            fontWeight={500}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            CONNECT WITH US
-          </Typography>
-          <Link
-            color={theme.palette.footer.dark}
-            href="https://www.yelp.com/biz/home-shine-solutions-somerville"
-            rel="noopener noreferrer"
-            target="_blank"
-            sx={{
-              "&:hover": {
-                color: theme.palette.footer.main,
-              },
-            }}
-          >
-            <Icon
-              baseClassName="fa-brands"
-              className="fa-yelp"
-              sx={{
-                color: theme.palette.footer.dark,
-                "&:hover": {
-                  color: theme.palette.footer.main,
-                },
-              }}
-            />
-          </Link>
-          <Link
-            color={theme.palette.footer.dark}
-            href="https://www.facebook.com/profile.php?id=100095591493494&mibextid=LQQJ4d"
-            rel="noopener noreferrer"
-            target="_blank"
-            sx={{
-              "&:hover": {
-                color: theme.palette.footer.main,
-              },
-            }}
-          >
-            <FacebookOutlinedIcon />
-          </Link>
+        Privacy Policy
+      </Link>
+      <Typography sx={{ mx: 1 }}>|</Typography>
+      <Link
+        href="/Terms/"
+        color={theme.palette.footer.dark}
+        underline="none"
+        sx={{
+          "&:hover": {
+            color: theme.palette.footer.main,
+          },
+        }}
+      >
+        Terms & Conditions
+      </Link>
+    </>
+  );
+}
+          <LogoPrivacyTerms />
         </Grid>
         <Grid
           xs={4}
