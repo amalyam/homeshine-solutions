cb3615947132421acad3cfaea7f36d2a2e178747
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-12-04 18:14:48 +0000

    add meta tag for viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
           src="https://kit.fontawesome.com/d0af135fd0.js"
           crossOrigin="anonymous"
