36015408c0c096111d329bce5f3d7a9d5524dac3
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-01-12 17:05:39 +0000

    add spacing

--- src/app/api/contact/emailBodyHtml.ts
+++ src/app/api/contact/emailBodyHtml.ts
@@ -14,6 +14,7 @@ export default ({
 <br />
 Thank you for contacting HomeShine Solutions, LLC. A member of our team will be in touch shortly to provide you with your FREE QUOTE!<br/>
 We do our best to respond within 24 hours.<br />
+<br />
 Below is a copy of your responses:<br />
 <br />
 Name: ${name}<br />
@@ -26,6 +27,7 @@ Services of interest: ${services ?? "[none selected]"} <br />
 Referral Source(s): ${referralSource ?? "[none selected]"}<br />
 <br />
 If you would like to update or change any of the information you provided, please reply to this email.<br />
+<br/>
 Have an excellent day!<br />
 <br />
 -- The HomeShine Team<br />
